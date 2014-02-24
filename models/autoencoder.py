721e4d4e81d4ef6d0ff7249bc9f3b07b3f8669b9
superjom <superjom@gmail.com>
2014-02-24 15:31:25 +0800

    add a train function to autoencoder

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -6,16 +6,19 @@ Created on Aug 9, 2013
 @author: Chunwei Yan @ pkusz
 @mail:  yanchunwei@outlook.com
 '''
-from __future__ import division
 import numpy
 import theano
 from theano import tensor as T
 
 class AutoEncoder(object):
-    def __init__(self, numpy_rng, input=None, n_visible=8, n_hidden=4,
+    def __init__(self, numpy_rng=None, input=None, n_visible=8, n_hidden=4,
             W=None, bhid=None, bvis=None):
         self.n_visible = n_visible
         self.n_hidden = n_hidden
+
+        if not numpy_rng:
+            numpy_rng=numpy.random.RandomState(1234)
+
         if not W:
             initial_W = numpy.asarray(
                 numpy_rng.uniform(
@@ -39,7 +42,8 @@ class AutoEncoder(object):
         self.W_prime = self.W.T
 
         if input == None:
-            self.X = T.dmatrix(name='input')
+            #self.X = T.dmatrix(name='input')
+            self.X = T.vector(name='X', dtype=theano.config.floatX)
         else:
             self.X = input
 
@@ -65,6 +69,21 @@ class AutoEncoder(object):
             updates.append((param, param - learning_rate * gparam))
         return cost, updates
 
+    def train(self, data=None, n_iters=1000, learning_rate=0.1):
+        n_features, n_items = data.shape
+        # compile function
+        cost, updates = self.get_cost_updates(learning_rate=0.1)
+        trainer = theano.function([self.X], cost, updates=updates)
+        for i in range(n_iters):
+            costs = []
+            for j in range(n_items):
+                d = data[j]
+                cost = trainer(d)
+                costs.append(cost)
+            print i, 'cost', numpy.mean(numpy.array(costs))
+
+
+
 
 
 
@@ -80,18 +99,25 @@ if __name__ == "__main__":
             print i, 'cost', cost
 
 
+    def train_one():
+        autoencoder = AutoEncoder(numpy_rng=numpy.random.RandomState(1234), n_visible=60, n_hidden=30)
+        autoencoder.X = T.vector(name='X', dtype=theano.config.floatX)
+        data = rng.randn(400, 60).astype(theano.config.floatX)
+        cost, updates = autoencoder.get_cost_updates(learning_rate=0.1)
+        train = theano.function([autoencoder.X], cost, updates=updates)
+        for i in range(40000):
+            costs = []
+            for j in range(400):
+                d = data[j]
+                cost = train(d)
+                costs.append(cost)
+            print i, 'cost', numpy.mean(numpy.array(costs))
+
     autoencoder = AutoEncoder(numpy_rng=numpy.random.RandomState(1234), n_visible=60, n_hidden=30)
-    autoencoder.X = T.vector(name='X', dtype=theano.config.floatX)
     data = rng.randn(400, 60).astype(theano.config.floatX)
-    cost, updates = autoencoder.get_cost_updates(learning_rate=0.1)
-    train = theano.function([autoencoder.X], cost, updates=updates)
-    for i in range(40000):
-        costs = []
-        for j in range(400):
-            d = data[j]
-            cost = train(d)
-            costs.append(cost)
-        print i, 'cost', numpy.mean(numpy.array(costs))
+
+    autoencoder.train(data)
+
 
 
 
