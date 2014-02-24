157a021ac95f539d7eb816de0f43fbfb8949dd5f
superjom <superjom@gmail.com>
2014-02-24 16:48:30 +0800

    change time

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Created on Aug 9, 2013
+Created on Feb 23, 2014
 
-@author: Chunwei Yan @ pkusz
+@author: Chunwei Yan @ PKU
 @mail:  yanchunwei@outlook.com
 '''
 import numpy
