b0da63d37501231011d610f7f48b6685b5a8423a
superjom <superjom@gmail.com>
2014-03-07 13:23:22 +0800

    lowerwise the words

--- apps/paper/dataset.py
+++ apps/paper/dataset.py
@@ -7,40 +7,31 @@ Created on March 7, 2014
 @mail:  yanchunwei@outlook.com
 '''
 from __future__ import division
+import nltk
 
 
 class DUC(object):
     def __init__(self, path):
         self.path = path
 
-    def get_text(self):
-        '''
-        get lines of the TEXT
-        '''
-        content = []
-        with open(self.path) as f:
-            begin = False
-            for line in f.readlines():
-                if line.find('<TEXT>') != -1:
-                    begin = True
-                elif line.find('</TEXT>') != -1:
-                    break
-                elif begin == True:
-                    content.append(line.strip())
-        return content
-
     def get_sentences(self):
         '''
         :return: 
             list of sentences(str)
         '''
-        content = ' '.join(self.get_text())
-        sentences = content.split('.')
+        with open(self.path) as f:
+            sentences = [s.strip() for s in f.readlines()]
         return sentences
+
+    def split_words(self, sentence):
+        return [w.lower() for w in nltk.word_tokenize(sentence)]
 
 
 
 
 if __name__ == '__main__':
-    duc = DUC('/home/chunwei/Lab/NeuralNetworks/apps/paper/data/duc2005_docs/d301i/FT921-10162')
-    duc.get_sentences()
+    duc = DUC('/home/chunwei/Lab/NeuralNetworks/apps/paper/data/duc06/sent_raw/D0601A/APW19990707.0181.sent')
+
+    sentences = duc.get_sentences()
+    for s in sentences:
+        print duc.split_words(s)
