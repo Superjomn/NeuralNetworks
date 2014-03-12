39bbec63f2d9dca9d9e460bec8db8f58f2650062
superjom <superjom@gmail.com>
2014-03-12 09:29:58 +0800

    add exe.sh

--- apps/paper/exe.sh
+++ apps/paper/exe.sh
@@ -5,8 +5,18 @@ set -x
 
 PROJECT_ROOT=/home/chunwei/Lab/NeuralNetworks/apps/paper
 DATA_ROOT=$PROJECT_ROOT/data
+CLEAN_DATA_PATH=$DATA_ROOT/clean.sentences.txt
 
 DATA_PH=${DATA_ROOT}/duc06/sent_raw
+
+# clean the DUC data
+clean_data()
+{
+    DUC_path=${DATA_ROOT}/DUC
+    paths=`find \`pwd\` -name *.sent`
+    cd $PROJECT_ROOT
+    cat $paths |  ./clean_sentence.py > $CLEAN_DATA_PATH
+}
 
 
 train_word2vec()
@@ -34,5 +44,6 @@ gen_syntax_tree()
 }
 
 
+#clean_data
 #train_word2vec
 gen_syntax_tree
